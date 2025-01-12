d776810d4b8a637a18f240935358281453fd1c97
Kaixn Zhang <kaixin2022mel@gmail.com>
2025-01-12 18:16:42 +0800

    Injected properties

--- kevinapp/src/main/java/com/web/springboot/demo/kevinapp/rest/FunRestController.java
+++ kevinapp/src/main/java/com/web/springboot/demo/kevinapp/rest/FunRestController.java
@@ -1,10 +1,27 @@
 package com.web.springboot.demo.kevinapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose a new endpoint for "team info"
+    @GetMapping("/teaminfo")
+    public String getTeamName() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
+
     // expose "/" that return "Hello world"
     @GetMapping("/")
     public String sayHello(){
