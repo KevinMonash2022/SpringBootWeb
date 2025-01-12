c53f80f9e75b1b816629856da7f13cb310b7162f
Kaixn Zhang <kaixin2022mel@gmail.com>
2025-01-12 13:40:17 +0800

    Init project

---
+++ kevinapp/src/main/java/com/web/springboot/demo/kevinapp/KevinappApplication.java
@@ -0,0 +1,13 @@
+package com.web.springboot.demo.kevinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinappApplication.class, args);
+	}
+
+}
---
+++ kevinapp/src/test/java/com/web/springboot/demo/kevinapp/KevinappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.springboot.demo.kevinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
