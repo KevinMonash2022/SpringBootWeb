2e269a8c5b1d2bdd2b0d434a10026293ff6b4040
Kaixn Zhang <kaixin2022mel@gmail.com>
2025-01-14 18:53:45 +0800

    Init spring boot(restcontroller)
    https://jsonplaceholder.typicode.com/users

---
+++ 04-SpringBootRestCURD/01-restcurd/src/main/java/com/kevin/restcurd/RestcurdApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.restcurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcurdApplication.class, args);
+	}
+
+}
